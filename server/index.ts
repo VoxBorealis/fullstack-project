5962298a2f9d6c61648340c7c1d4781ab79ebe63
Vox Borealis <uusitalo.y@gmail.com>
2023-02-26 20:10:03 +0200

    separate backend from frontend and configure build scripts

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import path from 'path';
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static('build'));
+// Path to the static React content, it changes if we build this ts file..
+const buildPath = __dirname.includes('dist') ? '../build' : 'build';
+
+app.use(express.static(path.join(__dirname, buildPath)));
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
